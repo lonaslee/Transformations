5a5713df7926dfd2a4339ff49e05551db2cc999a
lonaslee <93890846+lonaslee@users.noreply.github.com>
2022-03-04 19:58:42 -0600

    Update transformations.py

--- transformations.py
+++ transformations.py
@@ -1,49 +1,23 @@
 from __future__ import annotations
-from math import sqrt
+from math import sqrt, sin, cos, radians
+from re import search, findall
 
 
 class Point:
-    def __init__(self, x: float = 0.0, y: float = 0.0) -> None:
+    def __init__(self, x: float,
+                 y: float, name: str = '') -> None:
         self.x = float(x)
         self.y = float(y)
+        self.name = name
 
     def __str__(self) -> str:
-        return f'{round(self.x, 2), round(self.y, 2)}'
+        return f'{self.name}{round(self.x, 2), round(self.y, 2)}'
 
     def __bool__(self) -> bool:
         if self.x and self.y:
             return True
         else:
             return False
-    
-    def translate(self, shift: Point):
-        original = Point(self.x, self.y)
-        self.x, self.y = self.x + shift.x, self.y + shift.y
-        return original
-
-    def reflect(self, line_of_refl: Equation):
-        original = Point(self.x, self.y)
-        if line_of_refl.rslope == 'undef':
-            shift = abs(self.x - line_of_refl.m)  # If x is to the right of the
-            if self.x >= line_of_refl.m:          # line, then shift has to be
-                shift *= -1                       # negative for the next point
-            self.x = line_of_refl.m + shift
-        elif line_of_refl.rslope == 0:
-            shift = abs(self.y - line_of_refl.m)
-            if self.y >= line_of_refl.m:
-                shift *= -1
-            self.y = line_of_refl.m + shift
-        else:
-            perp_line = Line(self, None, None, line_of_refl.m ** -1 * -1).eq
-            intersection = perp_line == line_of_refl
-            len_from_point = Line(self, intersection).length
-            new_points = Line(intersection, None,
-                              len_from_point, perp_line.m).b
-            if str(new_points[0]) == str(self):
-                self.x, self.y = new_points[1].x, new_points[1].y
-            else:
-                self.x, self.y = new_points[0].x, new_points[0].y
-        return original
 
 
 class Line:
@@ -56,20 +30,18 @@ class Line:
 
     @property
     def length(self) -> float:
-        if not self._length:
+        if self._length is None:
             x2, y2 = self.b.x - self.a.x, self.b.y - self.a.y
             self._length = sqrt(x2 * x2 + y2 * y2)
         return self._length
 
     @property
     def slope(self) -> float | str:
-        if not self._slope:
+        if self._slope is None:
             if d := self.b.x - self.a.x == 0:
                 self._slope = 'undef'
-            elif n := self.b.y - self.a.y == 0:
-                self._slope = 0.0
             else:
-                self._slope = n / d
+                self._slope = (self.b.y - self.a.y) / d
         return self._slope
 
     @property
@@ -103,14 +75,15 @@ class Line:
 class Equation:
     def __init__(self, eq: str) -> None:
         self.eq = self.clean_eq(eq)
-        self.rslope = 'undef' if 'x=' in eq else 0 if 'x' not in eq else None
+        self.rslope = ('undef' if 'x=' in self.eq else
+                       0 if 'x' not in self.eq else None)
 
     def __repr__(self) -> str:
         rep = f'{self.eq} -> {self.m} -> {self.rslope}'
         if not self.rslope:
             rep += f' -> {self.b}'
         return rep
-    
+
     @staticmethod
     def clean_eq(eq: str):
         eq = eq.strip().lower().replace(' ', '').replace('=x', '=1.0x')
@@ -120,7 +93,7 @@ class Equation:
 
     @property
     def m(self) -> float:
-        if self.rslope:
+        if self.rslope is not None:
             return float(self.eq[2:])
         else:
             return float(self.eq[2:self.eq.index('x')])
@@ -130,63 +103,161 @@ class Equation:
         return float(self.eq[self.eq.index('x') + 1:].lstrip('+'))
 
     def __eq__(self, other: Equation):
-        if other.rslope == 'undef':
-            if self.rslope == 0:
+        x, y = float(), float()
+        match other.rslope, self.rslope:
+            case 'undef', 0:
                 x, y = other.m, self.m
-            else:  # self.rslope is None
+            case 'undef', _:
                 x = other.m
                 y = self.m * x + self.b
-        elif other.rslope == 0:
-            if self.rslope == 'undef':
+            case 0, 'undef':
                 x, y = other.m, self.m
-            else:  # self.rslope is None
+            case 0, _:
                 y = other.m
                 x = (y - self.b) / self.m
-        else:  # other.rslope is None
-            if not self.rslope:
+            case _, _:
                 mval, bval = self.m - other.m, other.b - self.b
                 x = bval / mval
                 y = self.m * x + self.b
-            elif self.rslope == 'undef':
+            case _, 'undef':
                 x = self.m
                 y = other.m * x + other.b
-            else:  # self.rslope is False
+            case _, 0:
                 y = self.m
                 x = (y - other.b) / other.m
         return Point(x, y)
 
 
+class AllPoints:
+    def __init__(self) -> None:
+        self.allpoints = self.in_points()
+
+    @staticmethod
+    def in_points() -> list[Point]:
+        print('points: ')
+        pl = []
+        while nx := input('-> ').strip():
+            pl.append(nx)
+            print(nx)
+        for key, str_p in enumerate(pl):
+            m = search(r'(?P<n>[a-zA-Z])?[\s,]*?(?P<x>-?\d+)[\s,]*?(?P<y>-?\d+)', str_p)
+            pl[key] = Point(m.group('x'), m.group('y'), m.group('n') if m.group('n') else '')
+        return pl
+
+    def translate_all(self, shift: Point) -> None:
+        print('translations:')
+        for key, pt in enumerate(self.allpoints):
+            self.allpoints[key].x, self.allpoints[key].y = pt.x + shift.x, pt.y + shift.y
+
+    def reflect_all(self, line_of_ref: Equation) -> None:
+        print('reflections:')
+        refl_dict = {'undef': self.refl_undef, 0: self.refl_zero}
+        functype = refl_dict.get(line_of_ref.rslope, self.refl_else)
+        for pt in self.allpoints:
+            functype(pt, line_of_ref)
+
+    @staticmethod
+    def refl_undef(pt, lor) -> None:
+        shift = abs(pt.x - lor.m)  # If x is to the right of the
+        if pt.x >= lor.m:  # line of ref, then shift has to be
+            shift *= -1  # negative for the next point
+        pt.x = lor.m + shift
+
+    @staticmethod
+    def refl_zero(pt, lor) -> None:
+        shift = abs(pt.y - lor.m)
+        if pt.y >= lor.m:
+            shift *= -1
+        pt.y = lor.m + shift
+
+    @staticmethod
+    def refl_else(pt, lor) -> None:
+        perp_line = Line(pt, None, None, lor.m ** -1 * -1).eq
+        intersection = perp_line == lor
+        len_from_point = Line(pt, intersection).length
+        new_points = Line(intersection, None, len_from_point, perp_line.m).b
+        if str(new_points[0]) == str(pt):
+            pt.x, pt.y = new_points[1].x, new_points[1].y
+        else:
+            pt.x, pt.y = new_points[0].x, new_points[0].y
+
+    def rotate_all(self, args: tuple[Point, float]) -> None:
+        print('rotations:')
+        pt_of_rot, ang = args[0], radians(args[1])
+        for pt in self.allpoints:
+            pt.x, pt.y = (pt_of_rot.x + ((pt.x - pt_of_rot.x) * cos(ang) - (pt.y - pt_of_rot.y) * sin(ang)),
+                          pt_of_rot.y + ((pt.x - pt_of_rot.x) * sin(ang) + (pt.y - pt_of_rot.y) * cos(ang)))
+
+    def dilate_all(self, args: tuple[Point, float]) -> None:
+        print('dilations:')
+        point_of_dil, scale = args[0], args[1]
+        for pt in self.allpoints:
+            pt.x, pt.y = (point_of_dil.x + scale * (pt.x - point_of_dil.x),
+                          point_of_dil.y + scale * (pt.y - point_of_dil.y))
+
+
+class Inputs:
+    def main(self):
+        while True:
+            name_dict = {
+                'T': 'translate_all',
+                'r': 'reflect_all',
+                'R': 'rotate_all',
+                'D': 'dilate_all'
+            }
+            type_dict = {
+                'T': (lambda x, y: Point(x, y)),
+                'r': (lambda e: Equation(e)),
+                'R': (lambda x, y, r: (Point(x, y), radians(r))),
+                'D': (lambda x, y, s: (Point(x, y), float(s)))
+            }
+            this_oper = self.in_val()
+            if len(this_oper) != 1:
+                print(this_oper)
+                match_list = findall(r'[TRrD]\([\d, y=x.+-]+?\)', this_oper)
+                match_list.reverse()
+                for t in match_list:
+                    func_args = self.rematch(t, t[0])
+                    yield [name_dict[t[0]], type_dict[t[0]](*func_args)]
+            else:
+                this_args = self.in_val(this_oper)
+                func_args = self.rematch(this_args, this_oper)
+                yield [name_dict[this_oper[0]], type_dict[this_oper[0]](*func_args)]
+
+    @staticmethod
+    def in_val(t: str = '') -> str:
+        msgs = {
+            'T': ('translation: ',),
+            'r': ('line of reflection: ',),
+            'R': ('point of rotation: ', 'angle of rotation: '),
+            'D': ('point of dilation: ', 'scale factor: ')
+        }
+        inp = ''
+        for m in msgs.get(t, ('transformation: ',)):
+            new = input(m).strip()
+            inp = ' '.join([inp, new]).strip()
+        return inp
+
+    @staticmethod
+    def rematch(s, m) -> tuple[str, ...]:
+        match m:
+            case 'T':
+                return search(r'([\d.-]+)[, ]+([\d.-]+)', s).groups()
+            case 'r':
+                return search(r'[\dy=x.+-]+', s).group(),
+            case _:
+                return search(r'([\d.-]+)[, ]+([\d.-]+)[, ]+([\d.-]+)', s).groups()
+
+
 def main():
-    points_list = []
-    print('Getting all points.')
-    while True:
-        input_point = input('Point: ').strip().strip('()').replace(',', ' ')
-        while '  ' in input_point:
-            input_point = input_point.replace('  ', ' ')
-        input_point = input_point.split(' ', 2)
-        try:
-            input_point = Point(input_point[0], input_point[1])
-            print(input_point)
-            points_list.append(input_point)
-        except IndexError:
-            break
-    operation = input('Operations - {T: translate, r: reflect\nOperator: ')
-    if operation == 'T':
-        t_amount = (input('Amount to translate: ').strip()
-                   .strip('()').replace(',', ' '))
-        while '  ' in t_amount:
-            t_amount = t_amount.replace('  ', ' ')
-        t_amount = t_amount.split(' ', 2)
-        t_amount = Point(t_amount[0], t_amount[1])
-        for p in points_list:
-            p.translate(t_amount)
-    elif operation == 'r':
-        r_line = Equation(input('Equation of the line of reflection: '))
-        for p in points_list:
-            p.reflect(r_line)
-    for p in points_list:
+    n = AllPoints()
+    for p in n.allpoints:
         print(p)
+    for func, args in Inputs().main():
+        getattr(n, func)(args)
+        for p in n.allpoints:
+            print(p)
 
 
-if __name__ == '__main__':
+if __name__ == 'main':
     main()
